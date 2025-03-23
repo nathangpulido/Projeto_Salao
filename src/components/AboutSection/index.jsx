faff24f5ecd748f8bdabbb4cb66de590df157f1b
nathangpulido <nathang.pulido98@outlook.com>
2025-03-23 17:54:15 -0300

    feat(nathan): Alterado de sessão sobre

--- src/components/AboutSection/index.jsx
+++ src/components/AboutSection/index.jsx
@@ -15,24 +15,7 @@ export default function AboutSection() {
       <div className={styles.text_content}>
         <h1 className={styles.title}>Sobre nós</h1>
         <p className={styles.paragraph}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-          sagittis arcu quis nisi luctus, id accumsan felis tristique. Proin
-          quis bibendum diam. Sed consequat nisl laoreet eros ultricies
-          pellentesque. Nullam in est porta, pellentesque massa vitae, vehicula
-          risus.
-        </p>
-        <p className={styles.paragraph}>
-          In placerat, felis vitae sodales dictum, lacus quam pretium mi, ut
-          pretium urna turpis eu dui. Vestibulum id ullamcorper nibh. Donec
-          luctus, nunc finibus elementum suscipit, tortor augue vulputate
-          sapien, vitae feugiat enim augue sed.
-        </p>
-        <p className={styles.paragraph}>
-          Quisque id aliquam elit. Suspendisse congue pharetra maximus. Duis
-          rutrum velit a leo euismod dictum. Sed sodales est efficitur arcu
-          tincidunt tincidunt. Curabitur fringilla, risus at feugiat feugiat,
-          nisl nulla tincidunt tellus, elementum elementum lorem nisl eleifend
-          dolor. Nullam eget dui at sem ullamcorper luctus.
+        Bem-vindos, aqui você encontra um espaço dedicado ao cuidado e à beleza de cada pessoa que entra por nossa porta. Nosso salão oferece uma experiência única, onde a estética se encontra com o bem-estar, e cada cliente é tratado com carinho, respeito e atenção personalizada..
         </p>
       </div>
       <GradientLine />
--- src/components/StartSection/index.jsx
+++ src/components/StartSection/index.jsx
@@ -13,10 +13,10 @@ export default function StartSection() {
         alt="Mulheres em um salão. Uma está atendendo com uma escova e um secador de cabelo na mão"
       />
       <div className={styles.text_content}>
-        <h1 className={styles.title}>Saúde natural para os seus cabelos</h1>
+        <h1 className={styles.title}>Saúde natural para a sua autoestima</h1>
         <p className={styles.paragraph}>
           Um salão exclusivo em São Paulo, especializado em tratamentos
-          naturais.
+          estéticos.
         </p>
         <a href="#contact" className={styles.ancor}>
           Agendar um horário
